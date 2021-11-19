5d6423d1b9ca85c5f748013ebafc604b87b90396
bkfwebdev <bryant.feld@gmail.com>
2021-11-19 11:56:22 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require("express")
+const app = express();
+const itemsRoutes = require("./routes/items")
+const ExpressError = require("./expressError")
+let port = 3000
+
+app.use(express.json());
+app.use("/items", itemsRoutes);
+
+/** 404 handler */
+
+app.use(function (req, res, next) {
+  return new ExpressError("Not Found", 404);
+});
+
+/** general error handler */
+
+app.use((err, req, res, next) => {
+  res.status(err.status || 500);
+
+  return res.json({
+    error: err.message,
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+});
+
+module.exports = app
